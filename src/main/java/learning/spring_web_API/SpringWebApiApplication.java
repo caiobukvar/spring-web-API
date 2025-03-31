01a4eff2bb77197f0adef48ebab2d77fb2b66520
Caio Bukvar <bukvar2009@gmail.com>
2025-03-31 17:21:50 -0300

    Criando projeto spring web, desenvolvendo uma API RESTful.

---
+++ src/main/java/learning/spring_web_API/SpringWebApiApplication.java
@@ -0,0 +1,13 @@
+package learning.spring_web_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApiApplication.class, args);
+	}
+
+}
